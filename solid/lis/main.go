1d64f33e41525c84700e3bce80c318132aaf1292
Kirill Kotliarov <kirill.kotliarov@synesis.ru>
2017-08-02 12:33:33 +0300

    solid in go: liskov substitution

---
+++ solid/lis/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Liskov ...
+type Liskov interface {
+	LiskovFunc()
+}
+
+// Parent ...
+type Parent struct{}
+
+// Child ...
+type Child struct {
+	Parent
+}
+
+// LiskovFunc ...
+func (p *Parent) LiskovFunc() {
+	fmt.Println("It works")
+}
+
+// LiskovSubstitution ...
+func LiskovSubstitution(lis Liskov) {
+	lis.LiskovFunc()
+}
+
+func main() {
+	ch := &Child{}
+	LiskovSubstitution(ch)
+}
